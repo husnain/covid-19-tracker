c6f3c7c2a8913c262ccd7863beec0aaa7f47d30b
Husnain Ahmed <husnain.ahmed@skillorbit.com>
2020-07-03 16:00:50 +0500

    removed unused files

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,21 +1,21 @@
 import React, {useState, useEffect} from 'react';
-import { InputLabel ,makeStyles ,Select,NativeSelect, FormControl} from '@material-ui/core';
+import { InputLabel ,Select, FormControl} from '@material-ui/core';
 import styles from './CountryPicker.module.css';
 import { fetchCountries } from '../../api';
 
-const useStyles = makeStyles((theme) => ({
-    formControl: {
-      margin: theme.spacing(1),
-      minWidth: 120,
-    },
-    selectEmpty: {
-      marginTop: theme.spacing(2),
-    },
-  }));
+// const useStyles = makeStyles((theme) => ({
+//     formControl: {
+//       margin: theme.spacing(1),
+//       minWidth: 120,
+//     },
+//     selectEmpty: {
+//       marginTop: theme.spacing(2),
+//     },
+//   }));
 
 const CountryPicker = ({handleCountryChange}) => {
 
-    const classes = useStyles();
+    // const classes = useStyles();
     const [fetchedCountries, setFetchedCountries] = useState([]);
     useEffect(() => {
         const fetchAPI = async () => {
